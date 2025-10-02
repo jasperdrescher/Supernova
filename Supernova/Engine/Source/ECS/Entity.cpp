a98ed41f80880cd0f86d7ebf249428c430941341
Jasper Drescher <jasperdre@gmail.com>
2025-10-02 17:21:32 +0900

    Add forward declaration for Scene

--- Supernova/Engine/Source/ECS/Entity.cpp
+++ Supernova/Engine/Source/ECS/Entity.cpp
@@ -2,12 +2,62 @@
 
 #include "Scene.hpp"
 
+#include <cassert>
 #include <entt.hpp>
 
 namespace ECS
 {
-	ECS::Entity::Entity(entt::entity aHandle, Scene* aScene)
+	Entity::Entity(entt::entity aHandle, Scene* aScene)
 		: mEntityHandle(aHandle), mScene(aScene)
 	{
+	}
+
+	template<typename T, typename... Args>
+	T& Entity::AddComponent(Args&&... aArgs)
+	{
+		assert(!HasComponent<T>());
+		T& component = mScene->mRegistry.emplace<T>(mEntityHandle, std::forward<Args>(aArgs)...);
+		mScene->OnComponentAdded<T>(*this, component);
+		return component;
+	}
+
+	template<typename T, typename... Args>
+	T& Entity::AddOrReplaceComponent(Args&&... aArgs)
+	{
+		T& component = mScene->mRegistry.emplace_or_replace<T>(mEntityHandle, std::forward<Args>(aArgs)...);
+		mScene->OnComponentAdded<T>(*this, component);
+		return component;
+	}
+
+	template<typename T>
+	T& Entity::GetComponent()
+	{
+		assert(HasComponent<T>());
+		return mScene->mRegistry.get<T>(mEntityHandle);
+	}
+
+	template<typename T>
+	const T& Entity::GetComponent() const
+	{
+		assert(HasComponent<T>());
+		return mScene->mRegistry.get<T>(mEntityHandle);
+	}
+
+	template<typename T>
+	const bool Entity::HasComponent() const
+	{
+		return mScene->mRegistry.all_of<T>(mEntityHandle);
+	}
+
+	template<typename T>
+	void Entity::RemoveComponent()
+	{
+		assert(HasComponent<T>());
+		mScene->mRegistry.remove<T>(mEntityHandle);
+	}
+
+	bool Entity::operator==(const Entity& aOther) const
+	{
+		return mEntityHandle == aOther.mEntityHandle && mScene == aOther.mScene;
 	}
 }
--- Supernova/Engine/Source/ECS/Entity.hpp
+++ Supernova/Engine/Source/ECS/Entity.hpp
@@ -1,10 +1,8 @@
 #pragma once
 
 #include "Components.hpp"
-#include "Scene.hpp"
 #include "UniqueIdentifier.hpp"
 
-#include <cassert>
 #include <cstdint>
 #include <entt.hpp>
 #include <string>
@@ -12,68 +10,41 @@
 
 namespace ECS
 {
+	class Scene;
+
 	class Entity
 	{
 	public:
 		Entity() = default;
 		Entity(entt::entity aHandle, Scene* aScene);
-		Entity(const Entity& aOthehr) = default;
+		Entity(const Entity& aOther) = default;
 
 		template<typename T, typename... Args>
-		T& AddComponent(Args&&... aArgs)
-		{
-			assert(!HasComponent<T>());
-			T& component = mScene->mRegistry.emplace<T>(mEntityHandle, std::forward<Args>(aArgs)...);
-			mScene->OnComponentAdded<T>(*this, component);
-			return component;
-		}
+		T& AddComponent(Args&&... aArgs);
 
 		template<typename T, typename... Args>
-		T& AddOrReplaceComponent(Args&&... args)
-		{
-			T& component = mScene->mRegistry.emplace_or_replace<T>(mEntityHandle, std::forward<Args>(args)...);
-			mScene->OnComponentAdded<T>(*this, component);
-			return component;
-		}
+		T& AddOrReplaceComponent(Args&&... aArgs);
 
 		template<typename T>
-		T& GetComponent()
-		{
-			assert(HasComponent<T>());
-			return mScene->mRegistry.get<T>(mEntityHandle);
-		}
+		T& GetComponent();
 
 		template<typename T>
-		const T& GetComponent() const
-		{
-			assert(HasComponent<T>());
-			return mScene->mRegistry.get<T>(mEntityHandle);
-		}
+		const T& GetComponent() const;
 
 		template<typename T>
-		const bool HasComponent() const
-		{
-			return mScene->mRegistry.all_of<T>(mEntityHandle);
-		}
+		const bool HasComponent() const;
 
 		template<typename T>
-		void RemoveComponent()
-		{
-			assert(HasComponent<T>());
-			mScene->mRegistry.remove<T>(mEntityHandle);
-		}
+		void RemoveComponent();
 
 		UniqueIdentifier GetUniqueIdentifier() const { return GetComponent<IDComponent>().mUniqueIdentifier; }
 		operator bool() const { return mEntityHandle != entt::null; }
 		operator entt::entity() const { return mEntityHandle; }
-		operator std::uint32_t() const { return (std::uint32_t)mEntityHandle; }
+		operator std::uint32_t() const { return static_cast<std::uint32_t>(mEntityHandle); }
 
 		const std::string& GetName() const { return GetComponent<TagComponent>().mTag; }
 
-		bool operator==(const Entity& aOther) const
-		{
-			return mEntityHandle == aOther.mEntityHandle && mScene == aOther.mScene;
-		}
+		bool operator==(const Entity& aOther) const;
 
 		bool operator!=(const Entity& aOther) const
 		{
