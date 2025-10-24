8b8ead341ce784be9a9fb83ca94b055422a49579
Jasper Drescher <jasperdre@gmail.com>
2025-10-24 17:56:11 +0900

    Add missing include

--- Supernova/Engine/Source/Math/Types.hpp
+++ Supernova/Engine/Source/Math/Types.hpp
@@ -4,6 +4,7 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/gtx/quaternion.hpp>
+#include <glm/mat2x2.hpp>
 #include <glm/mat3x3.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/vec2.hpp>
