efa48f66d16e353da2c0b286aa59b57324aa909a
Jasper Drescher <jasperdre@gmail.com>
2025-11-28 11:08:10 +0900

    Bump versions to latest

--- CMake/EnTT.cmake
+++ CMake/EnTT.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
 	EnTT
 	GIT_REPOSITORY https://github.com/skypjack/entt.git
-	GIT_TAG v3.15.0
+	GIT_TAG v3.16.0
 	GIT_SHALLOW TRUE)
 
 FetchContent_MakeAvailable(EnTT)
--- CMake/ImGui.cmake
+++ CMake/ImGui.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     imgui
     GIT_REPOSITORY https://github.com/ocornut/imgui.git
-    GIT_TAG v1.92.4
+    GIT_TAG v1.92.5
 	GIT_SHALLOW TRUE)
 
 FetchContent_MakeAvailable(imgui)
