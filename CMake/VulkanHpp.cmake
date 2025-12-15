026f5624089f52d613ce8e2f8e26ed9863f70efa
Jasper Drescher <jasperdre@gmail.com>
2025-12-15 11:40:13 +0900

    Use module again

--- CMake/VulkanHpp.cmake
+++ CMake/VulkanHpp.cmake
@@ -1,19 +1,20 @@
 find_package(Vulkan REQUIRED COMPONENTS glslc)
 find_program(glslc_executable NAMES glslc HINTS Vulkan::glslc)
 
-if(${Vulkan_VERSION} VERSION_LESS "1.3.256" )
+if(${Vulkan_VERSION} VERSION_LESS "1.3.256")
   message( FATAL_ERROR "Minimum required Vulkan version for C++ modules is 1.3.256. "
            "Found ${Vulkan_VERSION}.")
 endif()
 
-add_library(VulkanHpp INTERFACE)
+add_library(VulkanHppModule)
 
-target_include_directories(VulkanHpp INTERFACE ${Vulkan_INCLUDE_DIR})
+target_sources(VulkanHppModule PRIVATE
+  FILE_SET CXX_MODULES
+  BASE_DIRS ${Vulkan_INCLUDE_DIR}
+  FILES ${Vulkan_INCLUDE_DIR}/vulkan/vulkan.cppm)
 
-target_compile_definitions(VulkanHpp INTERFACE VULKAN_HPP_DISPATCH_LOADER_DYNAMIC=1)
+target_compile_features(VulkanHppModule PUBLIC cxx_std_20)
 
-target_compile_features(VulkanHpp INTERFACE cxx_std_20)
+target_link_libraries(VulkanHppModule PUBLIC Vulkan::Headers)
 
-target_link_libraries(VulkanHpp INTERFACE Vulkan::Headers)
-
-set_property(TARGET VulkanHpp PROPERTY FOLDER "Dependencies")
+set_property(TARGET VulkanHppModule PROPERTY FOLDER "Dependencies")
