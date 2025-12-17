4aee3045c4e1926ccc00aaee13ef3e25ce5572df
Jasper Drescher <jasperdre@gmail.com>
2025-12-17 12:30:23 +0900

    Use C++20 and Vulkan headers for Hpp

--- CMake/VulkanHpp.cmake
+++ CMake/VulkanHpp.cmake
@@ -5,18 +5,3 @@ if(${Vulkan_VERSION} VERSION_LESS "1.3.256")
   message( FATAL_ERROR "Minimum required Vulkan version for C++ modules is 1.3.256. "
            "Found ${Vulkan_VERSION}.")
 endif()
-
-add_library(VulkanHppModule)
-
-target_sources(VulkanHppModule PRIVATE
-  FILE_SET cxx_modules TYPE CXX_MODULES
-  BASE_DIRS ${Vulkan_INCLUDE_DIR}
-  FILES ${Vulkan_INCLUDE_DIR}/vulkan/vulkan.cppm)
-
-target_compile_features(VulkanHppModule PUBLIC cxx_std_23)
-
-target_include_directories(VulkanHppModule PRIVATE "${Vulkan_INCLUDE_DIR}")
-
-target_link_libraries(VulkanHppModule PUBLIC Vulkan::Headers)
-
-set_property(TARGET VulkanHppModule PROPERTY FOLDER "Dependencies")
